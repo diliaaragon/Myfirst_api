0337cea2abba1f509db0b7cffd222196974d3662
diliaaragon <dilia.aragon@koombea.com>
2019-06-07 14:43:43 -0500

    Add the validations to book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+  validates :title, precense: true, uniqueness: true
+  validates :edition, precense: true
+  validates :lenguage,  precense: true
+  validate :ISBN, precense: true
 end
