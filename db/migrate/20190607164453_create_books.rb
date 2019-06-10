5fe018b9e46336cfc1602ae0d53777223e5ad256
diliaaragon <dilia.aragon@koombea.com>
2019-06-10 14:00:13 -0500

    Add not null to attributes for book model

--- db/migrate/20190607164453_create_books.rb
+++ db/migrate/20190607164453_create_books.rb
@@ -1,12 +1,12 @@
 class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
-      t.string :title
+      t.string :title, null: false
       t.string :presentation
-      t.integer :edition
-      t.string :language
+      t.integer :edition, null: false
+      t.string :language, null: false
       t.integer :number_of_pages
-      t.integer :ISBN
+      t.integer :ISBN, null: false
 
       t.timestamps
     end
