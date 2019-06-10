a19a31e9c8ec9469ab3cb31b84fa727a3c3f052d
diliaaragon <dilia.aragon@koombea.com>
2019-06-10 10:54:10 -0500

    Add validation to the model publisher

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,2 +1,5 @@
 class Publisher < ApplicationRecord
+  validate :name, presence: true, uniqueness: true, length: { minimun: 4 }
+  validate :foundation, presence: true, numericality: { less_than_or_equal_to: 2019 }
+  validate :country, presence: true
 end
