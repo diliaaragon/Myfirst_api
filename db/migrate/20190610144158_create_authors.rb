72424b0091ff41c4d6d8e49c4295655b51965ecf
diliaaragon <dilia.aragon@koombea.com>
2019-06-10 09:45:07 -0500

    Create Author model

---
+++ db/migrate/20190610144158_create_authors.rb
@@ -0,0 +1,12 @@
+class CreateAuthors < ActiveRecord::Migration[5.2]
+  def change
+    create_table :authors do |t|
+      t.string :name
+      t.integer :age
+      t.string :natinality
+      t.integer :published_book
+
+      t.timestamps
+    end
+  end
+end
