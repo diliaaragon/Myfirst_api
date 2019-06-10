88507bf04058b55b911148a863f5c87e5f2f47a0
diliaaragon <dilia.aragon@koombea.com>
2019-06-10 15:16:11 -0500

    Add tests to the genre model

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Genre, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with name' do
+    genre = build(:genre)
+    expect(genre).to be_valid
+  end
+
+  it 'is invalid with name that already exist' do
+    genre_create = create(:genre, name: 'literary')
+    genre = build(:genre, name: 'literary')
+    expect(genre).to_not be_valid
+  end
 end
