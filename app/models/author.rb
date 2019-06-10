77fe01ecd448e0a8caaa0c63fb07905832f0045c
diliaaragon <dilia.aragon@koombea.com>
2019-06-10 10:15:50 -0500

    Add validation to the model Author

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ApplicationRecord
+  validate :first_name, presence: true, length: { minimun: 4 }
+  validate :last_name, presence: true, length: { minimun: 4 }
+  validate :age, presence: true
+  validate :natinality, presence: true
 end
