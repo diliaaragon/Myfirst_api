7403f80c1d2f724f4d53d134cc3fbc591fa4838a
diliaaragon <dilia.aragon@koombea.com>
2019-06-10 14:55:32 -0500

    Add test to the book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
   validates :title, presence: true
   validates :edition, presence: true
-  validates :lenguage,  presence: true
+  validates :language,  presence: true
   validates :ISBN, presence: true, uniqueness: true
 end
