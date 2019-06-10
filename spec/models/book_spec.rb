7428bb30ceb31acf20606ce98407563b0c636e4b
diliaaragon <dilia.aragon@koombea.com>
2019-06-10 14:55:32 -0500

    Add test to the book model

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with title, edition, lenguage, ISBN' do
+    book = build(:book)
+    expect(book).to be_valid
+  end
+
+  it 'is invalid without title' do
+    book = build(:book, title: nil)
+    expect(book).to_not be_valid
+  end
+
+  it 'is invalid without edition' do
+    book = build(:book, edition: nil)
+    expect(book).to_not be_valid
+  end
+
+  it 'is invalid without language' do
+    book = build(:book, language: nil)
+    expect(book).to_not be_valid
+  end
+
+  it 'is invalid without ISBN' do
+    book = build(:book, ISBN: nil)
+    expect(book).to_not be_valid
+  end
+
+  it 'is invalid with ISBN that already exist' do
+    book_create = create(:book, ISBN: 1234567891)
+    book = build(:book, ISBN: 1234567891)
+    expect(book).to_not be_valid
+  end
 end
