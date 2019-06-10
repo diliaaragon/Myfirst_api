295cab647278c7ccdaaa4e1a5897cb84a84db89a
diliaaragon <dilia.aragon@koombea.com>
2019-06-10 17:33:13 -0500

    fix errors

--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :author do
-    name { Faker::Book.author }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
     age { Faker::Number.between(20, 90) }
-    natinality { Faker::Nation.nationality }
+    nationality { Faker::Nation.nationality }
     published_book { Faker::Number.number(2) }
   end
 end
