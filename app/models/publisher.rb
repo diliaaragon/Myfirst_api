943788e582aab79b7d635d0af209417e5a2c29bf
diliaaragon <dilia.aragon@koombea.com>
2019-06-11 09:57:58 -0500

    Add tests to the publisher model

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,5 +1,5 @@
 class Publisher < ApplicationRecord
-  validate :name, presence: true, uniqueness: true, length: { minimun: 4 }
-  validate :foundation, presence: true, numericality: { less_than_or_equal_to: 2019 }
-  validate :country, presence: true
+  validates :name, presence: true, uniqueness: true, length: { minimum: 4 }
+  validates :foundation, presence: true, numericality: { less_than_or_equal_to: 2019 }
+  validates :country, presence: true
 end
