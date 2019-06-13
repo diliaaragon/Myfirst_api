9e0c09f68274d53269fc6a332bb9b53dd26b7831
diliaaragon <dilia.aragon@koombea.com>
2019-06-13 16:51:19 -0500

    Change the tests using shoulda-matchers

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,39 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  it 'is valid with first_name, las_name, age and nationality' do
-    author = build(:author)
-    expect(author).to be_valid
-  end
+  subject { FactoryBot.create(:author) }
 
-  it 'is invalid without first_name' do
-    author = build(:author, first_name: nil)
-    expect(author).to_not be_valid
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:first_name) }
+    it { is_expected.to validate_length_of(:first_name).is_at_least(4) }
+    it { is_expected.to validate_presence_of(:last_name) }
+    it { is_expected.to validate_length_of(:last_name).is_at_least(4) }
+    it { is_expected.to validate_presence_of(:age) }
+    it { is_expected.to validate_presence_of(:nationality) }
   end
-
-  it 'is invalid if first_name has less than four characters' do
-    author = build(:author, first_name: 'dan')
-    expect(author).to_not be_valid
-  end
-
-  it 'is invalid without last_name' do
-    author = build(:author, last_name: nil)
-    expect(author).to_not be_valid
-  end
-
-  it 'is invalid if last_name has less than four characters' do
-    author = build(:author, last_name: 'dan')
-    expect(author).to_not be_valid
-  end
-
-  it 'is invalid without age' do
-    author = build(:author, age: nil)
-    expect(author).to_not be_valid
-  end
-
-  it 'is invalid without nationality' do
-    author = build(:author, nationality: nil)
-    expect(author).to_not be_valid
-  end
-
 end
