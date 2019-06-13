41af65ce5e3f550ba8b79389473e00c06b753bce
diliaaragon <dilia.aragon@koombea.com>
2019-06-13 16:18:20 -0500

    Change the tests using shoulda-matchers

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,34 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  it 'is valid with title, edition, lenguage, ISBN' do
-    book = build(:book)
-    expect(book).to be_valid
-  end
+  subject { FactoryBot.create(:book) }
 
-  it 'is invalid without title' do
-    book = build(:book, title: nil)
-    expect(book).to_not be_valid
-  end
-
-  it 'is invalid without edition' do
-    book = build(:book, edition: nil)
-    expect(book).to_not be_valid
-  end
-
-  it 'is invalid without language' do
-    book = build(:book, language: nil)
-    expect(book).to_not be_valid
-  end
-
-  it 'is invalid without ISBN' do
-    book = build(:book, ISBN: nil)
-    expect(book).to_not be_valid
-  end
-
-  it 'is invalid with ISBN that already exist' do
-    book_create = create(:book, ISBN: 1234567891)
-    book = build(:book, ISBN: 1234567891)
-    expect(book).to_not be_valid
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:edition) }
+    it { is_expected.to validate_presence_of(:language) }
+    it { is_expected.to validate_presence_of(:ISBN) }
+    it { is_expected.to validete_uniqueness_of(:ISBN) }
   end
 end
