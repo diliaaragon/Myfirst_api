c2c7f69c10bc58df88d4d5bcffad6f417087c850
diliaaragon <dilia.aragon@koombea.com>
2019-06-13 17:04:43 -0500

    Change the tests using shoulda-matchers

--- spec/models/publisher_spec.rb
+++ spec/models/publisher_spec.rb
@@ -1,39 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Publisher, type: :model do
-  it 'is valid with name, foundation, country' do
-    publisher = build(:publisher)
-    expect(publisher).to be_valid
-  end
-
-  it 'is invalid without name' do
-    publisher = build(:publisher, name: nil)
-    expect(publisher).to_not be_valid
-  end
-
-  it 'is invalid if name that less than four characters' do
-    publisher = build(:publisher, name: 'num')
-    expect(publisher).to_not be_valid
-  end
-
-  it 'is invalid if the name already exist' do
-    publisher_create = create(:publisher, name: 'norma')
-    publisher = build(:publisher, name: 'norma')
-    expect(publisher).to_not be_valid
-  end
-
-  it 'is invalid without foundation' do
-    publisher = build(:publisher, foundation: nil)
-    expect(publisher).to_not be_valid
-  end
-
-  it 'is invalid if the year of foundation is greater than 2019' do
-    publisher = build(:publisher, foundation: 2020)
-    expect(publisher).to_not be_valid
-  end
-
-  it 'is invalid without country' do
-    publisher = build(:publisher, country: nil)
-    expect(publisher).to_not be_valid
+  subject { FactoryBot.create(:publisher) }
+  describe  'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+    it { is_expected.to validate_length_of(:name).is_at_least(4) }
+    it { is_expected.to validate_presence_of(:foundation) }
+    it { is_expected.to validate_numericality_of(:foundation).is_less_than_or_equal_to(2019)}
   end
 end
