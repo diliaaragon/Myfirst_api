d7975fed323210761d7487227f8f7827522e55d0
diliaaragon <dilia.aragon@koombea.com>
2019-06-20 16:34:59 -0500

    Generate user controller and add its methods

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+  include Response
+  include ExceptionHandler
+
+  before_action :set_user, only: [ :show, :update, :destroy]
+  def index
+    @users = User.all
+    json_response(@users)
+
+  end
+
+  def create
+    @user = User.create!(params_user)
+    json_response(@user)
+  end
+
+  def show
+    json_response(@user)
+  end
+
+  def show
+    json_response(@publisher)
+  end
+
+  def update
+    @user.update(user_params)
+    head :no_content
+  end
+
+  def destroy
+    @user.destroy
+    head :no_content
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = user.find(params[:id])
+  end
+end
